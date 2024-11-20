f42ca3697d3be8e2b48a687026989baf4195e735
Felix <felix@thiiia.com>
2024-11-20 14:46:57 +0000

    Update testerScript.cs

--- Assets/Scripts/testerScript.cs
+++ Assets/Scripts/testerScript.cs
@@ -7,7 +7,7 @@ public class testerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.log('Hey Hey Hey');
+      
     }
 
     // Update is called once per frame

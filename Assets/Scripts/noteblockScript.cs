178b496ad1394476534b2e71bfbea82fc8a873aa
SaheedKazeem <saheekazeem@gmail.com>
2024-11-29 23:19:47 +0000

    Added DoTween on Noteblocks

--- Assets/Scripts/noteblockScript.cs
+++ Assets/Scripts/noteblockScript.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class noteblockScript : MonoBehaviour
+public class NoteBlockScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public XplorerGuitarInput RefToInputController;
+    public GameObject[] RefToNoteblocks;
+
+    Color defaultGreenColor, defaultRedColor, defaultYellowColor, defaultBlueColor;
+    Tween greenTween, redTween, yellowTween, blueTween; 
+
     void Start()
     {
-        
+        // Initialize default colors ^^
+        defaultGreenColor = RefToNoteblocks[0].GetComponent<SpriteRenderer>().color;
+        defaultRedColor = RefToNoteblocks[1].GetComponent<SpriteRenderer>().color;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleColorChange(RefToInputController.green, RefToNoteblocks[0], ref greenTween, new Color(4f / 255f, 170f / 255f, 0f, 1f), defaultGreenColor);
+        HandleColorChange(RefToInputController.red, RefToNoteblocks[1], ref redTween, new Color(201f / 255f, 20f / 255f, 20f / 255f, 1f), defaultRedColor);
+    }
+
+    // handling color 
+    void HandleColorChange(bool isActive, GameObject noteBlock, ref Tween currentTween, Color targetColor, Color defaultColor)
+    {
+        SpriteRenderer sr = noteBlock.GetComponent<SpriteRenderer>();
+
+        // If input is active and not already animating to the target color
+        if (isActive && sr.color != targetColor)
+        {
+            
+            currentTween?.Kill();
+            currentTween = sr.DOColor(targetColor, 0.75f);
+        }
+        // If input is inactive and not already animating back to the default color
+        else if (!isActive && sr.color != defaultColor)
+        {
+            currentTween?.Kill();
+            currentTween = sr.DOColor(defaultColor, 0.75f);
+        }
     }
 }

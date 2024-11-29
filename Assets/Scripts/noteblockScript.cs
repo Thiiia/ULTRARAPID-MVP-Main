577d43fbc46bb9153bce839b99bd8a4fd4cc4758
SaheedKazeem <saheekazeem@gmail.com>
2024-11-29 23:36:31 +0000

    Button Interactions

--- Assets/Scripts/noteblockScript.cs
+++ Assets/Scripts/noteblockScript.cs
@@ -16,12 +16,16 @@ public class NoteBlockScript : MonoBehaviour
         // Initialize default colors ^^
         defaultGreenColor = RefToNoteblocks[0].GetComponent<SpriteRenderer>().color;
         defaultRedColor = RefToNoteblocks[1].GetComponent<SpriteRenderer>().color;
+        defaultYellowColor = RefToNoteblocks[2].GetComponent<SpriteRenderer>().color;
+        defaultBlueColor = RefToNoteblocks[3].GetComponent<SpriteRenderer>().color;
     }
 
     void Update()
     {
         HandleColorChange(RefToInputController.green, RefToNoteblocks[0], ref greenTween, new Color(4f / 255f, 170f / 255f, 0f, 1f), defaultGreenColor);
         HandleColorChange(RefToInputController.red, RefToNoteblocks[1], ref redTween, new Color(201f / 255f, 20f / 255f, 20f / 255f, 1f), defaultRedColor);
+        HandleColorChange(RefToInputController.yellow, RefToNoteblocks[2], ref yellowTween, new Color(245f / 255f, 185f / 255f, 13 / 255f, 1f), defaultYellowColor);
+        HandleColorChange(RefToInputController.blue, RefToNoteblocks[3], ref blueTween, new Color(7f / 255f, 101f / 255f, 234f / 255f, 1f), defaultBlueColor);
     }
 
     // handling color 
@@ -34,13 +38,13 @@ public class NoteBlockScript : MonoBehaviour
         {
             
             currentTween?.Kill();
-            currentTween = sr.DOColor(targetColor, 0.75f);
+            currentTween = sr.DOColor(targetColor, 0.45f);
         }
         // If input is inactive and not already animating back to the default color
         else if (!isActive && sr.color != defaultColor)
         {
             currentTween?.Kill();
-            currentTween = sr.DOColor(defaultColor, 0.75f);
+            currentTween = sr.DOColor(defaultColor, 0.45f);
         }
     }
 }

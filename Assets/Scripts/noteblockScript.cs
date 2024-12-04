931bb3946ca8d349ecaf5d1225890d3bc994bd5a
SaheedKazeem <saheekazeem@gmail.com>
2024-12-04 17:43:07 +0000

    adding factor tree

--- Assets/Scripts/noteblockScript.cs
+++ Assets/Scripts/noteblockScript.cs
@@ -9,20 +9,27 @@ public class NoteBlockScript : MonoBehaviour
     public GameObject[] RefToNoteblocks;
 
     Color defaultGreenColor, defaultRedColor, defaultYellowColor, defaultBlueColor;
+    Vector3 originalScaleGreen, originalScaleRed, originalScaleYellow, originalScaleBlue; // Store original scales
     Tween greenTween, redTween, yellowTween, blueTween;
 
     Color defaultDPadLeftColor, defaultDPadRightColor, defaultDPadUpColor, defaultDPadDownColor;
+    Vector3 originalScaleDPadLeft, originalScaleDPadRight, originalScaleDPadUp, originalScaleDPadDown; // Store original scales
     Tween dpadLeftTween, dpadRightTween, dpadUpTween, dpadDownTween;
 
     void Start()
     {
-        // Initializing default colors for Noteblocks
+        // Initializing default colors and original scales for Noteblocks
         if (this.gameObject.name == "GNoteblocks")
         {
             defaultGreenColor = RefToNoteblocks[0].GetComponent<SpriteRenderer>().color;
             defaultRedColor = RefToNoteblocks[1].GetComponent<SpriteRenderer>().color;
             defaultYellowColor = RefToNoteblocks[2].GetComponent<SpriteRenderer>().color;
             defaultBlueColor = RefToNoteblocks[3].GetComponent<SpriteRenderer>().color;
+
+            originalScaleGreen = RefToNoteblocks[0].transform.localScale;
+            originalScaleRed = RefToNoteblocks[1].transform.localScale;
+            originalScaleYellow = RefToNoteblocks[2].transform.localScale;
+            originalScaleBlue = RefToNoteblocks[3].transform.localScale;
         }
         
         if (this.gameObject.name == "DNoteblocks")
@@ -31,6 +38,11 @@ public class NoteBlockScript : MonoBehaviour
             defaultDPadRightColor = RefToNoteblocks[1].GetComponent<SpriteRenderer>().color;
             defaultDPadUpColor = RefToNoteblocks[2].GetComponent<SpriteRenderer>().color;
             defaultDPadDownColor = RefToNoteblocks[3].GetComponent<SpriteRenderer>().color;
+
+            originalScaleDPadLeft = RefToNoteblocks[0].transform.localScale;
+            originalScaleDPadRight = RefToNoteblocks[1].transform.localScale;
+            originalScaleDPadUp = RefToNoteblocks[2].transform.localScale;
+            originalScaleDPadDown = RefToNoteblocks[3].transform.localScale;
         }
     }
 
@@ -39,38 +51,43 @@ public class NoteBlockScript : MonoBehaviour
         // if this is the GNoteblocks gameobject, run thine code my good sir
         if (this.gameObject.name == "GNoteblocks")
         {
-            HandleColorChange(RefToInputController.green, RefToNoteblocks[0], ref greenTween, new Color(4f / 255f, 170f / 255f, 0f, 1f), defaultGreenColor);
-            HandleColorChange(RefToInputController.red, RefToNoteblocks[1], ref redTween, new Color(201f / 255f, 20f / 255f, 20f / 255f, 1f), defaultRedColor);
-            HandleColorChange(RefToInputController.yellow, RefToNoteblocks[2], ref yellowTween, new Color(245f / 255f, 185f / 255f, 13f / 255f, 1f), defaultYellowColor);
-            HandleColorChange(RefToInputController.blue, RefToNoteblocks[3], ref blueTween, new Color(7f / 255f, 101f / 255f, 234f / 255f, 1f), defaultBlueColor);
+            HandleColorAndAnimation(RefToInputController.green, RefToNoteblocks[0], ref greenTween, new Color(4f / 255f, 170f / 255f, 0f, 1f), defaultGreenColor, originalScaleGreen);
+            HandleColorAndAnimation(RefToInputController.red, RefToNoteblocks[1], ref redTween, new Color(201f / 255f, 20f / 255f, 20f / 255f, 1f), defaultRedColor, originalScaleRed);
+            HandleColorAndAnimation(RefToInputController.yellow, RefToNoteblocks[2], ref yellowTween, new Color(245f / 255f, 185f / 255f, 13f / 255f, 1f), defaultYellowColor, originalScaleYellow);
+            HandleColorAndAnimation(RefToInputController.blue, RefToNoteblocks[3], ref blueTween, new Color(7f / 255f, 101f / 255f, 234f / 255f, 1f), defaultBlueColor, originalScaleBlue);
         }
 
         // Likewise for DNoteblocks
         if (this.gameObject.name == "DNoteblocks")
         {
-            HandleColorChange(RefToInputController.DPadLeft > 0, RefToNoteblocks[0], ref dpadLeftTween, new Color(4f / 255f, 170f / 255f, 0f, 1f), defaultDPadLeftColor); 
-            HandleColorChange(RefToInputController.DPadUp > 0, RefToNoteblocks[1], ref dpadRightTween, new Color(201f / 255f, 20f / 255f, 20f / 255f, 1f), defaultDPadRightColor);
-            HandleColorChange(RefToInputController.DPadDown > 0, RefToNoteblocks[2], ref dpadUpTween, new Color(245f / 255f, 185f / 255f, 13f / 255f, 1f), defaultDPadUpColor); 
-            HandleColorChange(RefToInputController.DPadRight > 0, RefToNoteblocks[3], ref dpadDownTween, new Color(7f / 255f, 101f / 255f, 234f / 255f, 1f), defaultDPadDownColor);
+            HandleColorAndAnimation(RefToInputController.DPadLeft > 0, RefToNoteblocks[0], ref dpadLeftTween, new Color(4f / 255f, 170f / 255f, 0f, 1f), defaultDPadLeftColor, originalScaleDPadLeft); 
+            HandleColorAndAnimation(RefToInputController.DPadUp > 0, RefToNoteblocks[1], ref dpadRightTween, new Color(201f / 255f, 20f / 255f, 20f / 255f, 1f), defaultDPadRightColor, originalScaleDPadRight);
+            HandleColorAndAnimation(RefToInputController.DPadDown > 0, RefToNoteblocks[2], ref dpadUpTween, new Color(245f / 255f, 185f / 255f, 13f / 255f, 1f), defaultDPadUpColor, originalScaleDPadUp); 
+            HandleColorAndAnimation(RefToInputController.DPadRight > 0, RefToNoteblocks[3], ref dpadDownTween, new Color(7f / 255f, 101f / 255f, 234f / 255f, 1f), defaultDPadDownColor, originalScaleDPadDown);
         }
     }
 
-    // handling color
-    void HandleColorChange(bool isActive, GameObject noteBlock, ref Tween currentTween, Color targetColor, Color defaultColor)
+    // handling color and animation to enhance satisfaction for button presses
+    void HandleColorAndAnimation(bool isActive, GameObject noteBlock, ref Tween currentTween, Color targetColor, Color defaultColor, Vector3 originalScale)
     {
         SpriteRenderer sr = noteBlock.GetComponent<SpriteRenderer>();
 
         // If input is active and not already animating to the target color
         if (isActive && sr.color != targetColor)
         {
-            currentTween?.Kill();
-            currentTween = sr.DOColor(targetColor, 0.45f);
+            currentTween?.Kill(); // Kill any existing tween to avoid stacking
+            currentTween = DOTween.Sequence()
+                .Append(noteBlock.transform.DOScale(originalScale * 1.2f, 0.1f).SetEase(Ease.OutBack))  // Scale up with a nice ease-out effect for impact
+                .Join(sr.DOColor(targetColor, 0.2f).SetEase(Ease.Linear))  // Change color to the target color
+                .Append(noteBlock.transform.DOScale(originalScale, 0.2f).SetEase(Ease.OutBounce));  // Return to original scale with a bounce effect
         }
         // If input is inactive and not already animating back to the default color
         else if (!isActive && sr.color != defaultColor)
         {
-            currentTween?.Kill();
-            currentTween = sr.DOColor(defaultColor, 0.45f);
+            currentTween?.Kill(); // Kill any existing tween to avoid stacking
+            currentTween = DOTween.Sequence()
+                .Append(sr.DOColor(defaultColor, 0.2f).SetEase(Ease.Linear))  // Return to default color
+                .Join(noteBlock.transform.DOScale(originalScale, 0.1f));  // Ensure scale is reset smoothly
         }
     }
 }
